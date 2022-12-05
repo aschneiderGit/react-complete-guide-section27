f2cbab9c023275e872682765eb2105d651000960
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-05 13:48:18 +0100

    useTypeScript on component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Todos items={['Learn React', 'Learn TypeScript']} />
+		</div>
+	);
 }
 
 export default App;
